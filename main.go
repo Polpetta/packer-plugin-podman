8bf734b141de1f02b05aaf049e472d3f6479d718
Davide Polonio <davide.polonio@zextras.com>
2021-08-26 15:04:59 +0200

    chore: Additional project clean up

--- main.go
+++ main.go
@@ -4,9 +4,6 @@ import (
 	"fmt"
 	"os"
 	"packer-plugin-podman/builder/podman"
-	podmanData "packer-plugin-podman/datasource/podman"
-	podmanPP "packer-plugin-podman/post-processor/podman"
-	podmanProv "packer-plugin-podman/provisioner/podman"
 	podmanVersion "packer-plugin-podman/version"
 
 	"github.com/hashicorp/packer-plugin-sdk/plugin"
@@ -14,10 +11,7 @@ import (
 
 func main() {
 	pps := plugin.NewSet()
-	pps.RegisterBuilder("my-builder", new(podman.Builder))
-	pps.RegisterProvisioner("my-provisioner", new(podmanProv.Provisioner))
-	pps.RegisterPostProcessor("my-post-processor", new(podmanPP.PostProcessor))
-	pps.RegisterDatasource("my-datasource", new(podmanData.Datasource))
+	pps.RegisterBuilder("podman", new(podman.Builder))
 	pps.SetVersion(podmanVersion.PluginVersion)
 	err := pps.Run()
 	if err != nil {
